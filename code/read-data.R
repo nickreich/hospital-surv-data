5011a3c96c49d62c3348cb17d30385ef1ccc891b
Nicholas G Reich <nick@schoolph.umass.edu>
2014-11-06 16:00:30 -0500

    ignore and first code.

---
+++ code/read-data.R
@@ -0,0 +1,17 @@
+## data should be stored in hospital-surv-data/data
+
+require(reshape2)
+
+## load data
+dat <- read.csv("data/chco.csv")
+
+## fix dates
+dat$Date <- as.Date(dat$Date..Month.Year., "%m/%d/%y")
+dat <- dat[,-which(colnames(dat)=="Date..Month.Year.")]
+
+## melt data -- maybe should change to dplyr syntax!
+dat_melted <- melt(dat, id="Date")
+
+## nice all-pathogen plot
+qplot(x=Date, ymin=0, ymax=value, geom="linerange", data=dat_melted, color=variable) + 
+    facet_grid(variable~., scales="free_y") + theme(legend.position="none")
